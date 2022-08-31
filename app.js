f839c41bd4b939d092b3cd6ad0d9c19f4fdf9c02
swetabh-hoopy <102510219+swetabh-hoopy@users.noreply.github.com>
2022-09-01 02:46:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('hello world');
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('listening on port 3000');
 });
